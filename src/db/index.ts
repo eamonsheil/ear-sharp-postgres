9716b719a9d3a22dc55371c0a68adecba4aba2f0
Eamon Sheil <96795706+eamonsheil@users.noreply.github.com>
2022-12-09 12:41:37 -0500

    Revert "Auth"

--- src/db/index.ts
+++
@@ -1,9 +0,0 @@
-const { Pool } = require('pg');
-
-const pool = new Pool();
-
-module.exports = {
-    query: (text: any, params: any, callback: any) => {
-        return pool.query(text, params, callback)
-    }
-}
