2bea17e0439087b81ad88af857e37e62829966aa
Eamon Sheil <Eamondsheil@gmail.com>
2022-12-14 18:31:10 -0500

    jwt auth, add helmet

--- src/controllers/student.controller.ts
+++ src/controllers/student.controller.ts
@@ -1,6 +1,7 @@
 // const app = require('express');
 import { Request, Response } from "express";
 import * as bcrypt from 'bcrypt';
+import * as jwt from 'jsonwebtoken';
 import { CreateUserDto } from "../models/student.model";
 const db = require('../db');
 
@@ -15,43 +16,77 @@ const registerStudent = async (req: Request, res: Response) => {
         const query = `
             INSERT INTO students (name, email, password, created_on, last_login)
             VALUES ($1, $2, $3, current_timestamp, current_timestamp)
+            RETURNING *
         `;
         const values = [name, email, hashedPassword];
         const result = await db.query(query, values);
-        const user: CreateUserDto = result.rows[0];
-        console.log(user)
-        res.status(200).send(user);
+        const user = result.rows[0];
+
+        const token = generateToken(user.id);
+
+        res.cookie('access_token', token, {
+            httpOnly: true,
+            maxAge: 90_000_000 * 3,
+            signed: true
+        });
+
+        return res
+            .status(200)
+            .send({ ...user, token });
     } catch (err: any) {
-        res.status(401).json({ error: "email already registered" });
+        res.status(401).json(err.message);
     }
 };
 
 const loginStudent = async (req: Request, res: Response) => {
     const { email, password } = req.body;
-    console.log(req.body)
+    console.log(req.signedCookies['access_token'])
     try {
         const query = `
-        SELECT * FROM students
-        WHERE email = $1
+            SELECT * FROM students
+            WHERE email = $1
         `;
         const values = [email];
         const result = await db.query(query, values);
         const user = result.rows[0];
 
-        const isValid = await bcrypt.compare(password, user.password)
+        if (!user) {
+            throw new Error('Invalid Email')
+        }
+
+        const isValid = await bcrypt.compare(password, user.password);
         if (user && isValid) {
             await db.query(`
                 UPDATE students SET last_login=CURRENT_TIMESTAMP 
                 WHERE email=$1
             `, [email]);
-            return res.status(200).json(user);
-        } else {
-            return res.status(401).json({ error: 'Invalid email or password' });
-        }
 
+            const token = generateToken(user.id);
+
+            res.cookie('access_token', token, {
+                httpOnly: true,
+                maxAge: 90_000_000 * 3,
+                signed: true
+            });
+
+            return res
+                .status(200)
+                .send({ ...user, token: token });
+        } else {
+            throw new Error('Invalid Password')
+        }
     } catch (err: any) {
-        res.status(500).json({ error: err.message });
+        res.status(500).json(err.message);
     }
+}
+
+
+
+const generateToken = (id: string): string => {
+
+    return jwt.sign({ id }, process.env.JWT_SECRET as jwt.Secret, {
+        expiresIn: '3d'
+    })
 }
 
 const getStudent = async (req: Request, res: Response) => {
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express, { RequestHandler } from "express";
 const db = require('./db');
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+const helmet = require('helmet');
 require('dotenv').config();
 
 
@@ -11,12 +12,12 @@ const port = process.env.PORT || 3333;
 
 app.use(express.json() as RequestHandler);
 app.use(express.urlencoded({ extended: true }) as RequestHandler);
-app.use(cookieParser());
+app.use(cookieParser("secret"));
 app.use(cors());
+app.use(helmet());
 
 
 app.use('/api/student', require('./routes/student.routes'));
-
 
 
 app.get("/", async (req, res) => {
@@ -26,4 +27,9 @@ app.get("/", async (req, res) => {
 
 app.listen(port, async () => {
   console.log(`connected to database on port ${port}`);
+});
+
+
+process.on('unhandledRejection', error => {
+  console.error('unhandledRejection', error);
 });
